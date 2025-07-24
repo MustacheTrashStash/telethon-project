3ad66292c14f64b117296c0933bf25f58b4c290d
Matt Bush <mbush@cctv.org>
2025-07-24 14:10:28 -0400

    Make LiveStreamCard responsive: width fits screen up to 50em max

--- telethon-storefront/src/modules/home/components/LiveStreamCard.tsx
+++ telethon-storefront/src/modules/home/components/LiveStreamCard.tsx
@@ -32,7 +32,7 @@ const LiveStreamCard = () => {
       className="card"
       style={{
         fontFamily: "Montserrat, sans-serif",
-        width: "50em",
+        width: "min(100vw, 50em)",
         translate: "-6px -6px",
         background: "#883377",
         border: "3px solid #000000",
@@ -65,7 +65,7 @@ const LiveStreamCard = () => {
           margin:0,
           width:"100%",
           height:"30em"
-        }} src="https://www.youtube.com/embed/jmZnc7eFR9Q?list=PLljLFn4BZd2PL7Z6x2egbcjWr0ib3gnaQ&index=3" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        }} src="https://www.youtube.com/embed/jmZnc7eFR9Q?list=PLljLFn4BZd2PL7Z6x2egbcjWr0ib3gnaQ&index=3" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
 
       </div>
     </div>
