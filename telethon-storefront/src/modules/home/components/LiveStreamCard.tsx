b9d80fd29afcd21fab3d9d118223abf640cb20fc
Matt Bush <mbush@cctv.org>
2025-07-25 18:16:58 -0400

    feat: add debug logging for sold out product filtering and update LiveStreamCard styling

--- telethon-storefront/src/modules/home/components/LiveStreamCard.tsx
+++ telethon-storefront/src/modules/home/components/LiveStreamCard.tsx
@@ -65,7 +65,7 @@ const LiveStreamCard = () => {
           margin:0,
           width:"100%",
           height:"30em"
-        }} src="https://www.youtube.com/embed/jmZnc7eFR9Q?list=PLljLFn4BZd2PL7Z6x2egbcjWr0ib3gnaQ&index=3" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+        }} src="https://www.youtube.com/embed/s7nOhdDJzp8?si=l5s3Rz3qSqitGZzb" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 
       </div>
     </div>
